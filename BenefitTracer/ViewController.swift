95e0784f8b008ce8a5724e906471fd8c234189c9
Shao-Jie Tang <s510248@livemail.tw>
2021-02-20 17:08:30 +0800

    project init

---
+++ BenefitTracer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BenefitTracer
+//
+//  Created by Jay on 2021/2/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
