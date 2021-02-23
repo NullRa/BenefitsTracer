faebef7b48c3d73ee3903b8843d300f23a1fe2cb
Shao-Jie Tang <s510248@livemail.tw>
2021-02-23 08:35:27 +0800

    circularView test

--- BenefitTracer/View/MoneyViewController.swift
+++ BenefitTracer/View/MoneyViewController.swift
@@ -8,6 +8,9 @@
 import UIKit
 
 class MoneyViewController: UIViewController {
+    @IBOutlet weak var moneyTableView: UITableView!
+    @IBOutlet weak var circularLabel: UILabel!
+    @IBOutlet weak var circularView: UIView!
 
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
@@ -21,14 +24,35 @@ class MoneyViewController: UIViewController {
         initUI()
         bind()
         // Do any additional setup after loading the view.
+        addCircular()
     }
 
     func initUI(){
-
+        circularLabel.text = "Circular Progress Ring"
     }
 
     func bind(){
 
+    }
+
+    func addCircular(){
+        //底層的圓環
+        let circularPath = UIBezierPath(ovalIn: CGRect(x: 110, y: 20, width: 200, height: 200))
+        let circularLayer = CAShapeLayer()
+        circularLayer.path = circularPath.cgPath
+        circularLayer.strokeColor = UIColor.gray.cgColor
+        circularLayer.fillColor = UIColor.clear.cgColor
+        circularLayer.lineWidth = 25
+
+        let percentageCircularPath = UIBezierPath(arcCenter: CGPoint(x: 210, y: 120), radius: 100, startAngle: CGFloat.pi/180*270, endAngle: CGFloat.pi/180*360, clockwise: true)
+        let percentageCircularLayer = CAShapeLayer()
+        percentageCircularLayer.path = percentageCircularPath.cgPath
+        percentageCircularLayer.strokeColor = UIColor.blue.cgColor
+        percentageCircularLayer.fillColor = UIColor.clear.cgColor
+        percentageCircularLayer.lineWidth = 25
+
+        self.circularView.layer.addSublayer(circularLayer)
+        self.circularView.layer.addSublayer(percentageCircularLayer)
     }
     
 
