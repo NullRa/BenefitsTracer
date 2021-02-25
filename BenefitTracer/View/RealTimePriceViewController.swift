470d59a94adf740dbe8e97abcac1f591c3e27d9f
jyang <jTang@walkgame.com>
2021-02-25 14:10:00 +0800

    init realTimePriceViewController

---
+++ BenefitTracer/View/RealTimePriceViewController.swift
@@ -0,0 +1,57 @@
+//
+//  BenefitsViewController.swift
+//  BenefitTracer
+//
+//  Created by Jay on 2021/2/21.
+//
+
+import UIKit
+
+class RealTimePriceViewController: UIViewController {
+    let realTimePriceCellCellId = "realTimePriceCell"
+    var itemList: [ItemData] = []
+    let userRespository = UserRespository()
+    //$:1000(+0%)
+    //$:1300(+30%)
+    @IBOutlet weak var totalMoneyLabel: UILabel!
+    @IBOutlet weak var realTimePriceTableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        initUI()
+        bind()
+    }
+    
+    func initUI(){
+        totalMoneyLabel.text = "test"
+    }
+    
+    func bind(){
+        realTimePriceTableView.delegate = self
+        realTimePriceTableView.dataSource = self
+        setList()
+    }
+}
+
+extension RealTimePriceViewController {
+    func setList(){
+        let itemDict = userRespository.queryItemCoreData()
+        for (name,price) in itemDict {
+            let itemData = ItemData(itemName: name, itemPrice: price)
+            itemList.append(itemData)
+        }
+    }
+}
+
+extension RealTimePriceViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemList.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: realTimePriceCellCellId, for: indexPath)
+        cell.textLabel?.text = itemList[indexPath.row].itemName
+        cell.detailTextLabel?.text = "\(itemList[indexPath.row].itemPrice)"
+        return cell
+    }
+}
